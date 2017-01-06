d01740e0f98fa00d4bf683654adb15d2794f53ae
Albert <1014838101@qq.com>
2017-01-06 23:19:35 +0800

    新增admin模型

---
+++ jingCms/apps/admin/model/Article.php
@@ -0,0 +1,7 @@
+<?php
+namespace app\admin\model;
+
+class Article{
+	// 三种模式
+}
+?>
--- jingCms/apps/admin/model/User.php
+++ jingCms/apps/admin/model/User.php
@@ -12,28 +12,51 @@ class User
 		# code...
 	}
 
-	public function login()
-	{}
+	public function login(user)
+	{
+		// user 对象 包含账号、密码
+	}
 
-	public function register()
-	{}
+	public function register(user)
+	{
+		// user 手机/电子邮件、密码、性别、
+	}
 
-	public function logout()
-	{}
+	public function logout(uid)
+	{
+		// 用户编号
+	}
 
-	public function forget()
-	{}
+	public function forget(phone/email)
+	{
+		// 通过电子邮件/电话发送验证码
+		// 验证验证码是否正确
+		// 重新输入密码
+	}
 
-	public function init()
-	{}
+	public function init(user)
+	{
+		//更新初始化信息
+	}
 
-	public function active()
-	{}
+	public function active(user)
+	{
+		//更新激活信息
+	}
 
-	public function real()
-	{}
+	public function real(user)
+	{
+		// 验证真实身份
+	}
 
-	public function delete()
-	{}
+	public function delete(id, status)
+	{
+		// 删除/禁用用户
+	}
+
+	public function list(catgory, page, status = "1")
+	{
+		// 用户分类/分页数/状态
+	}
 }
 ?>
